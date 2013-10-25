0182ecb36a374cab2545f05b69b3277751709176
xpirapp <aplicativos@xpirapp.com.br>
2013-10-25 07:16:43 -0200

    Up3

--- assets/www/cordova_plugins.js
+++ assets/www/cordova_plugins.js
@@ -135,6 +135,13 @@ module.exports = [
         "clobbers": [
             "window.FileTransfer"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/InAppBrowser.js",
+        "id": "org.apache.cordova.inappbrowser.InAppBrowser",
+        "clobbers": [
+            "window.open"
+        ]
     }
 ]
 });
